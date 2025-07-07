ded4987b418ba1832c234cac4afea582c34e0bdc
FabioHOliver <fabiohoffoliver@gmail.com>
2025-07-07 19:29:15 -0300

    arrumando erros.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// codigo js da pagina
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Mensagem enviada com sucesso!");
+      form.reset();
+    });
+  }
+});
